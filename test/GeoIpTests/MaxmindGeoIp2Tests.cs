9cfbb734b9a9ad1f6afbd0f29190940ab4dca9f8
heny <henrik.nystrom@episerver.com>
2018-05-11 12:50:11 +1200

    Convert test to xUnit.net

--- test/GeoIpTests/MaxmindGeoIp2Tests.cs
+++ test/GeoIpTests/MaxmindGeoIp2Tests.cs
@@ -1,22 +1,21 @@
-﻿using NUnit.Framework;
-using Pixie.Extensions.Maxmind.GeoIp.Services;
+﻿using Pixie.Extensions.Maxmind.GeoIp.Services;
 using System.Collections.Specialized;
 using System.Net;
+using Xunit;
 
 namespace GeoIpTests
 {
-    [TestFixture]
     public class MaxmindGeoIp2Tests
     {
         private GeolocationMaxmindService service = new GeolocationMaxmindService();
 
-        [TestCase]
+        [Fact]
         public void MaxmindServiceDBTest()
         {
             NameValueCollection config = new NameValueCollection();
             config.Add("databaseFileName", @"C:\Pixie\Research\EPiServer\Maxmind.GeoIP\GeoIpTests\db\GeoLite2-City.mmdb");
             var result = service.GetGeoLocation(IPAddress.Parse("213.205.251.152"), config);
-            Assert.AreEqual("GB", result.CountryCode);
+            Assert.Equal("GB", result.CountryCode);
         }
 
     }
